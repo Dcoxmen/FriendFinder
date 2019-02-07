a3e7ebffcd08fba791c88f2ff3c7624f2f373bce
Dave Cox <davedvst@gmail.com>
2019-02-07 09:14:20 -0800

    added api-routing code

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ var path = require("path");
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text);
+app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/api-routes")(app);
+require("./app/routing/html-routes")(app);
 
 app.listen(port, () => console.log("Listening on port %s", port));
